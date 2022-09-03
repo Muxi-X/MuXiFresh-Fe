79e54b07eb44b67c53343d28b8f28e434a9cc30a
MUMU46 <1819883988@qq.com>
2022-09-03 10:36:24 +0800

    change form

--- src/components/LogIn/index.tsx
+++ src/components/LogIn/index.tsx
@@ -6,9 +6,22 @@ import { useNavigate } from 'react-router-dom'
 import Join from '../../images/join.png'
 
 const LogIn = (props: any) => {
-    const [form,setForm]=useState();//报名表状态
+    const [form,setForm]=useState(0);//报名表状态
 
-
+   /*  useEffect(()=>{
+        getJson('/schedule')
+        .then(
+            data=>{
+                //0表示未提交 1表示提交
+                setForm(data.data.form_status);
+                console.log(data.data)
+                console.log('##',data.data.form_status)
+            }
+        )
+   .catch(error=>console.log(error))
+    },[]
+    )
+ */
     const {setIsLogIn} = props
 
     const [user,setUser] = useState({
@@ -65,28 +78,31 @@ const LogIn = (props: any) => {
             data=>{
                 //0表示未提交 1表示提交
                 setForm(data.data.form_status);
-                console.log('##',data.data.form_status)
-            }
-        )
-        .catch(error=>console.log(error))
+                console.log(data.data)
+                console.log('##',data.data.form_status);
 
-        getJson('/user/info')
-        .then(
-            data => {
-                if(data.data.role===1){//visitor
-                   const toVisitor = ()=>{
-                    navigate(form==0?'/edit':'/visitor')
-                   }
-                   toVisitor()
-                }
-                else if(data.data.role===3||data.data.role===4){
-                    const toManager = ()=>{//manager
-                        navigate(form==0?'/edit':'/manager')
+                getJson('/user/info')
+                .then(
+                    data => {
+                        if(data.data.role===1){//visitor
+                           const toVisitor = ()=>{
+                            navigate(data.data.form_status==0?'/edit':'/visitor')
+                           }
+                           toVisitor()
+                        }
+                        else if(data.data.role===3||data.data.role===4){
+                            const toManager = ()=>{//manager
+                                navigate(data.data.form_status==0?'/edit':'/manager')
+                            }
+                            toManager()
+                        }
                     }
-                    toManager()
-                }
+                )
             }
         )
+   .catch(error=>console.log(error))
+   
+       
     }
     
     const back = () => {
