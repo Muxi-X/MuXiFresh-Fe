44f7a714b8c14f2e9c581a0e49298b6d0b8207bb
lyxaaaa <2780197685@qq.com>
2022-09-07 22:44:17 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import { useRoutes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useNavigate, useRoutes } from 'react-router-dom'
+import { getJson } from './interface/fetch'
 import J_layout from './components/J_layout'
 import router from './router'
 
 
 function App() {
+  const navigate=useNavigate()
+
+  useEffect(()=>{
+      const token = localStorage.getItem('token')
+      if(!token){
+          navigate('/login')
+      }
+  },[])
+
   return useRoutes(router);
 }
 
